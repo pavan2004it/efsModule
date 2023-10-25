a5bb03306b68d1274c4b13b16300f0d4c756639c
Pavan Kumar <pavan_2004it@icloud.com>
2023-10-25 12:53:26 +0530

    added lifecycle blocks separately

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@ resource "aws_efs_file_system" "rp-efs-jobs" {
   throughput_mode = var.throughput_mode
   lifecycle_policy {
     transition_to_ia = var.transition_to_ia
+  }
+  lifecycle_policy {
     transition_to_primary_storage_class = var.transition_out_of_ia
   }
   tags = {
